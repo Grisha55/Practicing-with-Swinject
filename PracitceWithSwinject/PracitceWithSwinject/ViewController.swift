a22f0228982c998d6808c02578585b12e5b0acb2
Grigory <grishavinyar@icloud.com>
2022-08-07 10:01:19 +0300

    Initial commit

---
+++ PracitceWithSwinject/PracitceWithSwinject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracitceWithSwinject
+//
+//  Created by Григорий Виняр on 07/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
